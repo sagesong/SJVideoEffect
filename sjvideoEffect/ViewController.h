28f068b2c14cde362b6ba36574b46484e93d2e3f
lightning <songjianw@gmail.com>
2015-08-03 15:59:37 +0800

    Initial Commit

---
+++ sjvideoEffect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sjvideoEffect
+//
+//  Created by Lightning on 15/8/3.
+//  Copyright (c) 2015年 Lightning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
